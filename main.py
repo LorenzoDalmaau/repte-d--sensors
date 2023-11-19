2ac4c03096a48ca017749cf19d6db164df907a40
Lorenzo Poderoso Dalmau <136742186+LorenzoDalmaau@users.noreply.github.com>
2023-11-19 14:19:07 +0100

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+while True:
+    if input.temperature() > 22:
+        basic.show_string("Calor!")
+    else:
+        basic.show_string("Fred")
---
+++ main.ts
@@ -0,0 +1,7 @@
+while (true) {
+    if (input.temperature() > 22) {
+        basic.showString("Calor!")
+    } else {
+        basic.showString("Fred")
+    }
+}
